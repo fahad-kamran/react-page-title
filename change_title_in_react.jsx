17f8d4340ad3f895cff3f7c990f99d0f1f21c57d
enayatansari43 <enayat@xolva.com>
2021-12-23 10:35:03 +0500

    change title

---
+++ change_title_in_react.jsx
@@ -0,0 +1,29 @@
+
+import React, { useEffect } from "react";
+
+const ChangePageTitle = ({ pageTitle }) => {
+  useEffect(() => {
+    const prevTitle = document.title;
+    document.title = pageTitle;
+    return () => {
+      document.title = prevTitle;
+    };
+  });
+
+  return <></>;
+};
+
+export default ChangePageTitle;
+
+
+
+import ChangePageTitle from "../{yourLocation}/ChangePageTitle";
+
+...
+
+return (
+    <>
+      <ChangePageTitle pageTitle="theTitleYouWant" />
+      ...
+    </>
+  );
